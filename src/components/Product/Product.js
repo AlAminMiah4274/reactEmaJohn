11bbce343926898de68a86cd2fe9991d7e43498f
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-15 18:19:09 +0600

    done many thing

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    // console.log(props);
+    console.log(props.product);
     const { img, name, price, seller, ratings } = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: {price}</p>
+                <p><small>Manufacturer: {seller}</small></p>
+                <p><small>Rating: {ratings} stars</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add to Cart </p>
+            </button>
         </div>
     );
 };
