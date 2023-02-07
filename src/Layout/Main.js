b533da1706c783baee47ff63808461b7971e1381
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-02-07 18:33:38 +0600

    modified app.js and completed main.js

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from '../components/Header/Header';
+import Shop from '../components/Shop/Shop';
 
 const Main = () => {
     return (
         <div>
-
+            <Header></Header>
+            <Shop></Shop>
         </div>
     );
 };
