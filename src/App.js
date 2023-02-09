4f5321e4fe190b05e50679a335b72136c7d36391
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-02-09 11:17:30 +0600

    done many things

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
 import Main from './Layout/Main';
+import Shop from './components/Shop/Shop'
+import Orders from './components/Orders/Orders';
+import Inventory from './components/Inventory/Inventory';
+import { productsAndCartLoader } from './Loaders/ProductsAndCartLoader';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Main></Main>
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          loader: () => fetch('products.json'),
+          element: <Shop></Shop>
+        },
+        {
+          path: 'orders',
+          loader: productsAndCartLoader,
+          element: <Orders></Orders>
+        },
+        {
+          path: 'inventory',
+          element: <Inventory></Inventory>
+        }
+      ]
+    },
+    {
+      path: 'about',
+      element: <About></About>
     }
   ]);
   return (
